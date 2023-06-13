ee48ec5769fb050be52b5dee93c8f6a0b21362c2
Paweł Styka <107481814+pstyka@users.noreply.github.com>
2023-06-13 22:27:31 +0200

    Add files via upload

--- src/main/java/efs/task/reflection/ClassInspector.java
+++ src/main/java/efs/task/reflection/ClassInspector.java
@@ -1,8 +1,13 @@
 package efs.task.reflection;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ClassInspector {
 
@@ -15,11 +20,20 @@ public class ClassInspector {
    * @param annotation szukana adnotacja
    * @return lista zawierająca tylko unikalne nazwy pól oznaczonych adnotacją
    */
-  public static Collection<String> getAnnotatedFields(final Class<?> type,
-      final Class<? extends Annotation> annotation) {
-    //TODO usuń zawartość tej metody i umieść tutaj swoje rozwiązanie
-    return Collections.emptyList();
+  public static Collection<String> getAnnotatedFields(final Class<?> type, final Class<? extends Annotation> annotation) {
+    Set<String> set_annotated = new HashSet<>();
+    Field[] fields = type.getDeclaredFields();
+
+    for(Field f : fields) {
+      if(f.isAnnotationPresent(annotation)) {
+        set_annotated.add(f.getName());
+      }
+    }
+
+    return set_annotated;
   }
+    //TODO usuń zawartość tej metody i umieść tutaj swoje rozwiązanie
+
 
   /**
    * Metoda powinna wyszukać wszystkie zadeklarowane bezpośrednio w klasie metody oraz te
@@ -31,8 +45,23 @@ public class ClassInspector {
    * implementowane
    */
   public static Collection<String> getAllDeclaredMethods(final Class<?> type) {
-    //TODO usuń zawartość tej metody i umieść tutaj swoje rozwiązanie
-    return Collections.emptyList();
+    Set<String> declared_methods = new HashSet<>();
+    Method[] methods = type.getDeclaredMethods();
+
+    for (Method m : methods) {
+      declared_methods.add(m.getName());
+    }
+
+    Class<?>[] interfaces = type.getInterfaces();
+    for (Class<?> inter_face : interfaces) {
+      Method[] interfaceMethods = inter_face.getDeclaredMethods();
+
+      for (Method inter_face_method : interfaceMethods) {
+        declared_methods.add(inter_face_method.getName());
+      }
+    }
+
+    return declared_methods;
   }
 
   /**
@@ -50,7 +79,27 @@ public class ClassInspector {
    * @throws Exception wyjątek spowodowany nie znalezieniem odpowiedniego konstruktora
    */
   public static <T> T createInstance(final Class<T> type, final Object... args) throws Exception {
-    //TODO usuń zawartość tej metody i umieść tutaj swoje rozwiązanie
-    return null;
+    Constructor<T>[] constr = (Constructor<T>[]) type.getDeclaredConstructors();
+
+    for (Constructor<T> constructor : constr) {
+      if (constructor.getParameterCount() == args.length) {
+
+        boolean instance = true;
+        for (int i = 0; i < args.length; i++) {
+          if (!constructor.getParameterTypes()[i].isInstance(args[i])) {
+            instance = false;
+            break;
+          }
+        }
+
+        if (instance) {
+          constructor.setAccessible(true);
+
+          return constructor.newInstance(args);
+        }
+      }
+    }
+
+    throw new Exception("Can't find matching constructor!");
   }
 }
